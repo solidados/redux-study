1e02f2010effab2c7314facc1d02640df4739dfb
solidados <geekbrains.pk@internet.ru>
2024-04-30 15:31:22 +0400

    feat: prepare project structure on React+Vite+TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className={"app"}>
+      <div style={{ display: "flex" }}>
+        <button>Credit</button>
+        <button>Debit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
